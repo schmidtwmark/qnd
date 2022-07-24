1ce0744c441e4690a342b057cf8f4ab126ccd2cd
Mark Schmidt <mark.schmidt@hey.com>
2022-07-24 13:31:52 -0500

    Day 9

--- solutions/day9.py
+++ solutions/day9.py
@@ -1,51 +1,61 @@
 import random
+from termcolor import colored
+
 
 def verify_word(guess_word, secret_word):
-    if not dictionary.check(guess_word):
-        return None
+    correct = True
     output = ""
-
     for i in range(len(guess_word)):
         if guess_word[i] == secret_word[i]:
-            output += "🟩"
+            output += colored(guess_word[i], "green")
         elif guess_word[i] in secret_word:
-            output += "🟨"
+            output += colored(guess_word[i], "yellow")
+            correct = False
         else:
-            output += "⬜️"
+            output += guess_word[i]
+            correct = False
+    return correct, output
 
-    return output
 
 def get_random_word():
     word_list = ["apple", "pears", "tombs", "train"]
     return random.choice(word_list)
 
+
+def load_words():
+    with open("five_letter_words.txt") as f:
+        words = f.readlines()
+    return words
+
+
 def play():
     guess_number = 0
     word = get_random_word()
-    print("Welcome to Wordle! I have a secret word, you have 6 chances to guess it")
+    print(
+        "Welcome to Wordle! I have a secret word, you have 6 chances to guess it")
     while guess_number < 6:
-        guess_word = input("Enter a word: ")
-        verify_result = verify_word(guess_word, word)
-        if verify_result is None:
-            print("Invalid word")
-        elif verify_result == "🟩🟩🟩🟩🟩":
-            print("You win!")
+        guess_word = input("Enter a guess: ")
+        if guess_word not in valid_words:
+            print("Invalid word: {guess_word}, try again")
+            continue
+        verify_result, output = verify_word(guess_word, word)
+        print(output)
+        if verify_result:
+            print(f"You win in {guess_number} tries!")
             break
         else:
-            print(verify_word(guess_word, word))
             guess_number += 1
             if guess_number == 6:
                 print("You lose!")
                 break
-    return guess_number, word
+    return word, guess_number
 
-play_count = {}
+valid_words = load_words()
+guess_count = {}
 while True:
-    guesses, word = play()
-    play_again = input("Do you want to play again? (y/n) ")
-    if play_again == "y":
+    word, guesses = play()
+    guess_count[word] = guesses
+    if input("Play again? (y/n): ") == "y":
         continue
     else:
-        print(play_count)
-        break
-
+        print(f"Scores: {guess_count}")
